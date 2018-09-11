918fe2550f8cfba06d3716f09da8036f55b3026e
mimitch2 <mike@melodix.com>
2018-09-11 09:37:20 -0500

    inlineblock to make safari issue better

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ import '../css/App.css'
 const Header = () => {
   return (
     <header className="name-heading basic-flex-row">
+      <img src="img/camera.png" alt="" className="camera-image"/> 
       <h1 className="title">MIKE J MITCHELL PHOTOGRAPHY</h1> 
     </header>
   )
--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -11,12 +11,7 @@ const Info = (props) => {
       <p className="info-text">I designed and built this site myself to showcase some of my photos from over the years. I hope you enjoy them.</p>
 
       <p className="info-text">Check out my web developer portfolio site <a href="http://mikejmitchell.com/" target="_blank" rel='noopener noreferrer'>here</a>.
-      Thanks!</p>
-      
-      <img src="img/camera.png" alt="" className="camera-image"/> 
-
-      
-      
+      Thanks!</p>    
     </div>
   )
 }
