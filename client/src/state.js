4a68d27a4239280d8f512e61802ff2010e55bddf
mimitch2 <mike@melodix.com>
2019-03-31 19:54:40 -0500

    merge

--- client/src/state.js
+++ client/src/state.js
@@ -1,4 +1,3 @@
-
 export default {
   winSize: {},
   scrollPos: 0,
@@ -6,360 +5,368 @@ export default {
   pictures: [
     {
       id: 1,
-      thumb:"/img/mjm_thumb_ - 1.jpg",
-      full:"/img/mjm_ - 1.jpg",
-      year: "2010",
-      location: "Copperopolis, CA",
-      format: "Medium Format Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 1.jpg',
+      full: '/img/mjm_ - 1.jpg',
+      year: '2010',
+      location: 'Copperopolis, CA',
+      format: 'Medium Format Film',
+      notes: ''
     },
     {
       id: 38,
-      thumb:"/img/mjm_thumb_ - 38.jpg",
-      full:"/img/mjm_ - 38.jpg",
-      year: "2018",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 38.jpg',
+      full: '/img/mjm_ - 38.jpg',
+      year: '2018',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
+    },
+    {
+      id: 41,
+      thumb: '/img/mjm_thumb_ - 41.jpg',
+      full: '/img/mjm_ - 41.jpg',
+      year: '2018',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
 
     {
       id: 2,
-      thumb:"/img/mjm_thumb_ - 2.jpg",
-      full:"/img/mjm_ - 2.jpg",
-      year: "2010",
-      location: "Arnold, CA",
-      format: "Medium Format Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 2.jpg',
+      full: '/img/mjm_ - 2.jpg',
+      year: '2010',
+      location: 'Arnold, CA',
+      format: 'Medium Format Film',
+      notes: ''
     },
     {
       id: 24,
-      thumb:"/img/mjm_thumb_ - 24.jpg",
-      full:"/img/mjm_ - 24.jpg",
-      year: "2015",
-      location: "Paris, France",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 24.jpg',
+      full: '/img/mjm_ - 24.jpg',
+      year: '2015',
+      location: 'Paris, France',
+      format: 'Digital',
+      notes: ''
     },
 
     {
       id: 3,
-      thumb:"/img/mjm_thumb_ - 3.jpg",
-      full:"/img/mjm_ - 3.jpg",
-      year: "2009",
-      location: "Paia, HI",
-      format: "Medium Format Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 3.jpg',
+      full: '/img/mjm_ - 3.jpg',
+      year: '2009',
+      location: 'Paia, HI',
+      format: 'Medium Format Film',
+      notes: ''
     },
     {
       id: 4,
-      thumb:"/img/mjm_thumb_ - 4.jpg",
-      full:"/img/mjm_ - 4.jpg",
-      year: "2011",
-      location: "Gooding, ID",
-      format: "Medium Format Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 4.jpg',
+      full: '/img/mjm_ - 4.jpg',
+      year: '2011',
+      location: 'Gooding, ID',
+      format: 'Medium Format Film',
+      notes: ''
     },
     {
       id: 5,
-      thumb:"/img/mjm_thumb_ - 5.jpg",
-      full:"/img/mjm_ - 5.jpg",
-      year: "2011",
-      location: "Gooding, ID",
-      format: "Medium Format Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 5.jpg',
+      full: '/img/mjm_ - 5.jpg',
+      year: '2011',
+      location: 'Gooding, ID',
+      format: 'Medium Format Film',
+      notes: ''
     },
     {
       id: 6,
-      thumb:"/img/mjm_thumb_ - 6.jpg",
-      full:"/img/mjm_ - 6.jpg",
-      year: "2011",
-      location: "Gooding, ID",
-      format: "Medium Format Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 6.jpg',
+      full: '/img/mjm_ - 6.jpg',
+      year: '2011',
+      location: 'Gooding, ID',
+      format: 'Medium Format Film',
+      notes: ''
     },
     {
       id: 8,
-      thumb:"/img/mjm_thumb_ - 8.jpg",
-      full:"/img/mjm_ - 8.jpg",
-      year: "2012",
-      location: "Mountain Home, ID",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 8.jpg',
+      full: '/img/mjm_ - 8.jpg',
+      year: '2012',
+      location: 'Mountain Home, ID',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 7,
-      thumb:"/img/mjm_thumb_ - 7.jpg",
-      full:"/img/mjm_ - 7.jpg",
-      year: "2018",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 7.jpg',
+      full: '/img/mjm_ - 7.jpg',
+      year: '2018',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 9,
-      thumb:"/img/mjm_thumb_ - 9.jpg",
-      full:"/img/mjm_ - 9.jpg",
-      year: "2006",
-      location: "Santa Cruz, CA",
-      format: "35mm Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 9.jpg',
+      full: '/img/mjm_ - 9.jpg',
+      year: '2006',
+      location: 'Santa Cruz, CA',
+      format: '35mm Film',
+      notes: ''
     },
     {
       id: 10,
-      thumb:"/img/mjm_thumb_ - 10.jpg",
-      full:"/img/mjm_ - 10.jpg",
-      year: "2013",
+      thumb: '/img/mjm_thumb_ - 10.jpg',
+      full: '/img/mjm_ - 10.jpg',
+      year: '2013',
       location: "Murphy's, CA",
-      format: "Medium Format Film",
-      notes: ""
+      format: 'Medium Format Film',
+      notes: ''
     },
     {
       id: 11,
-      thumb:"/img/mjm_thumb_ - 11.jpg",
-      full:"/img/mjm_ - 11.jpg",
-      year: "2013",
-      location: "Belize",
-      format: "35mm Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 11.jpg',
+      full: '/img/mjm_ - 11.jpg',
+      year: '2013',
+      location: 'Belize',
+      format: '35mm Film',
+      notes: ''
     },
     {
       id: 12,
-      thumb:"/img/mjm_thumb_ - 12.jpg",
-      full:"/img/mjm_ - 12.jpg",
-      year: "2013",
-      location: "Belize",
-      format: "35mm Film",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 12.jpg',
+      full: '/img/mjm_ - 12.jpg',
+      year: '2013',
+      location: 'Belize',
+      format: '35mm Film',
+      notes: ''
     },
     {
       id: 13,
-      thumb:"/img/mjm_thumb_ - 13.jpg",
-      full:"/img/mjm_ - 13.jpg",
-      year: "2013",
-      location: "Cupertino, CA",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 13.jpg',
+      full: '/img/mjm_ - 13.jpg',
+      year: '2013',
+      location: 'Cupertino, CA',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 14,
-      thumb:"/img/mjm_thumb_ - 14.jpg",
-      full:"/img/mjm_ - 14.jpg",
-      year: "2014",
-      location: "Puerto Vallarta, Mexico",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 14.jpg',
+      full: '/img/mjm_ - 14.jpg',
+      year: '2014',
+      location: 'Puerto Vallarta, Mexico',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 15,
-      thumb:"/img/mjm_thumb_ - 15.jpg",
-      full:"/img/mjm_ - 15.jpg",
-      year: "2014",
-      location: "Cupertino, CA",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 15.jpg',
+      full: '/img/mjm_ - 15.jpg',
+      year: '2014',
+      location: 'Cupertino, CA',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 17,
-      thumb:"/img/mjm_thumb_ - 17.jpg",
-      full:"/img/mjm_ - 17.jpg",
-      year: "2015",
-      location: "Paris, France",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 17.jpg',
+      full: '/img/mjm_ - 17.jpg',
+      year: '2015',
+      location: 'Paris, France',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 18,
-      thumb:"/img/mjm_thumb_ - 18.jpg",
-      full:"/img/mjm_ - 18.jpg",
-      year: "2015",
-      location: "Lyon, France",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 18.jpg',
+      full: '/img/mjm_ - 18.jpg',
+      year: '2015',
+      location: 'Lyon, France',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 19,
-      thumb:"/img/mjm_thumb_ - 19.jpg",
-      full:"/img/mjm_ - 19.jpg",
-      year: "2015",
-      location: "Lyon, France",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 19.jpg',
+      full: '/img/mjm_ - 19.jpg',
+      year: '2015',
+      location: 'Lyon, France',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 20,
-      thumb:"/img/mjm_thumb_ - 20.jpg",
-      full:"/img/mjm_ - 20.jpg",
-      year: "2015",
-      location: "Lyon, France",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 20.jpg',
+      full: '/img/mjm_ - 20.jpg',
+      year: '2015',
+      location: 'Lyon, France',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 21,
-      thumb:"/img/mjm_thumb_ - 21.jpg",
-      full:"/img/mjm_ - 21.jpg",
-      year: "2015",
-      location: "Lyon, France",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 21.jpg',
+      full: '/img/mjm_ - 21.jpg',
+      year: '2015',
+      location: 'Lyon, France',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 22,
-      thumb:"/img/mjm_thumb_ - 22.jpg",
-      full:"/img/mjm_ - 22.jpg",
-      year: "2015",
-      location: "Barcelona, Spain",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 22.jpg',
+      full: '/img/mjm_ - 22.jpg',
+      year: '2015',
+      location: 'Barcelona, Spain',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 23,
-      thumb:"/img/mjm_thumb_ - 23.jpg",
-      full:"/img/mjm_ - 23.jpg",
-      year: "2015",
-      location: "San Gimignano, Italy",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 23.jpg',
+      full: '/img/mjm_ - 23.jpg',
+      year: '2015',
+      location: 'San Gimignano, Italy',
+      format: 'Digital',
+      notes: ''
     },
- 
+
     {
       id: 25,
-      thumb:"/img/mjm_thumb_ - 25.jpg",
-      full:"/img/mjm_ - 25.jpg",
-      year: "2016",
-      location: "Lafayette⁩, CO",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 25.jpg',
+      full: '/img/mjm_ - 25.jpg',
+      year: '2016',
+      location: 'Lafayette⁩, CO',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 26,
-      thumb:"/img/mjm_thumb_ - 26.jpg",
-      full:"/img/mjm_ - 26.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 26.jpg',
+      full: '/img/mjm_ - 26.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 27,
-      thumb:"/img/mjm_thumb_ - 27.jpg",
-      full:"/img/mjm_ - 27.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 27.jpg',
+      full: '/img/mjm_ - 27.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 28,
-      thumb:"/img/mjm_thumb_ - 28.jpg",
-      full:"/img/mjm_ - 28.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 28.jpg',
+      full: '/img/mjm_ - 28.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 29,
-      thumb:"/img/mjm_thumb_ - 29.jpg",
-      full:"/img/mjm_ - 29.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 29.jpg',
+      full: '/img/mjm_ - 29.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 30,
-      thumb:"/img/mjm_thumb_ - 30.jpg",
-      full:"/img/mjm_ - 30.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 30.jpg',
+      full: '/img/mjm_ - 30.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
 
     {
       id: 32,
-      thumb:"/img/mjm_thumb_ - 32.jpg",
-      full:"/img/mjm_ - 32.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 32.jpg',
+      full: '/img/mjm_ - 32.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 33,
-      thumb:"/img/mjm_thumb_ - 33.jpg",
-      full:"/img/mjm_ - 33.jpg",
-      year: "2017",
-      location: "Muskogee, OK",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 33.jpg',
+      full: '/img/mjm_ - 33.jpg',
+      year: '2017',
+      location: 'Muskogee, OK',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 34,
-      thumb:"/img/mjm_thumb_ - 34.jpg",
-      full:"/img/mjm_ - 34.jpg",
-      year: "2018",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 34.jpg',
+      full: '/img/mjm_ - 34.jpg',
+      year: '2018',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 35,
-      thumb:"/img/mjm_thumb_ - 35.jpg",
-      full:"/img/mjm_ - 35.jpg",
-      year: "2018",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 35.jpg',
+      full: '/img/mjm_ - 35.jpg',
+      year: '2018',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 36,
-      thumb:"/img/mjm_thumb_ - 36.jpg",
-      full:"/img/mjm_ - 36.jpg",
-      year: "2018",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 36.jpg',
+      full: '/img/mjm_ - 36.jpg',
+      year: '2018',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 37,
-      thumb:"/img/mjm_thumb_ - 37.jpg",
-      full:"/img/mjm_ - 37.jpg",
-      year: "2018",
-      location: "Over Texas",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 37.jpg',
+      full: '/img/mjm_ - 37.jpg',
+      year: '2018',
+      location: 'Over Texas',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 31,
-      thumb:"/img/mjm_thumb_ - 31.jpg",
-      full:"/img/mjm_ - 31.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 31.jpg',
+      full: '/img/mjm_ - 31.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
-  
+
     {
       id: 39,
-      thumb:"/img/mjm_thumb_ - 39.jpg",
-      full:"/img/mjm_ - 39.jpg",
-      year: "2017",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
+      thumb: '/img/mjm_thumb_ - 39.jpg',
+      full: '/img/mjm_ - 39.jpg',
+      year: '2017',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
     },
     {
       id: 40,
-      thumb:"/img/mjm_thumb_ - 40.jpg",
-      full:"/img/mjm_ - 40.jpg",
-      year: "2018",
-      location: "Austin, TX",
-      format: "Digital",
-      notes: ""
-    },
-    
-  ]}
-  
+      thumb: '/img/mjm_thumb_ - 40.jpg',
+      full: '/img/mjm_ - 40.jpg',
+      year: '2018',
+      location: 'Austin, TX',
+      format: 'Digital',
+      notes: ''
+    }
+  ]
+};
